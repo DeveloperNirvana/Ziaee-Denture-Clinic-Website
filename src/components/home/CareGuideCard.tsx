e1e6fd4383789c1ea3de1506fcf5ed5e203d761b
santoshkeshrinirvana <95208636+santoshkeshrinirvana@users.noreply.github.com>
2026-09-10 12:03:18 +0530

    care component
    care component

--- src/components/home/CareGuideCard.tsx
+++ src/components/home/CareGuideCard.tsx
@@ -16,6 +16,7 @@ interface CareGuideCardProps {
   index: number;
 }
 
+
 export default function CareGuideCard({ data, index }: CareGuideCardProps) {
   const number = index + 1;
   if (number === 1) {
